19c7b258ccdefd9b7b1fdb48fc58451bde1d7338
Jason Maliquid <jasonm2822@gmail.com>
2025-01-21 08:23:58 +0800

    🔧 Update form schema to include first, last, and middle name fields; enhance phone validation and add address field

--- src/lib/config/zodSchema.ts
+++ src/lib/config/zodSchema.ts
@@ -1,11 +1,19 @@
 import z from 'zod';
 
 export const FormSchema = z.object({
-	name: z.string().nonempty(),
+	firstName: z.string().nonempty(),
+	lastName: z.string().nonempty(),
+	middleName: z.string().nonempty(),
 	gender: z.string().nonempty(),
 	age: z.number().int().positive(),
 	email: z.string().email(),
-	phone: z.number().int().positive()
+	phone: z
+		.number()
+		.refine(
+			(num: number) => num.toString().startsWith('09') && num.toString().length === 11,
+			"Mobile number must be 11 digits and start with '09'"
+		),
+	address: z.string().nonempty()
 });
 
 export type FormSchema = z.infer<typeof FormSchema>;
